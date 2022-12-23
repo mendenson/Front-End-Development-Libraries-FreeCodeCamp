5acb8241164c0c38c0053cb106a2cf0639b8e787
Mendenson Mota <mendenson@gmail.com>
2022-12-23 16:54:53 -0700

    Update Project

--- Quote Machine/script.js
+++ Quote Machine/script.js
@@ -1,12 +1,16 @@
 function App() {
 
     const [quotes, setQuotes] = React.useState([]);
-    const [randomquote, setRandomQuote] = React.useState([]);
+    const [randomQuote, setRandomQuote] = React.useState("");
+    const [color, setColor] = React.useState("111")
 
     React.useEffect(() => {
         async function fetchData() {
-            const response = await fetch("quotes.json")
+            const response = await fetch("https://type.fit/api/quotes")
             const data = await response.json();
+        
+            
+            
 
             setQuotes(data);
             let randIndex = Math.floor(Math.random() * data.length);
@@ -15,12 +19,66 @@ function App() {
         fetchData();
     }, [])
 
-    return (
-        <div>Hello, World!
+    const getNewQuote = () => {
+        
+        const colors = [
+            "#16a085",
+            "#27ae60",
+            "#2c3e50",
+            "#f39c12",
+            "#e74c3c",
+            "#9b59b6",
+            "#FB6964",
+            "#342224",
+            "#472E32",
+            "#BDBB99",
+            "#77B1A9",
+            "#73A857",
+        ]
 
-            {quotes.map (quote => (
-                <div>{quote.text}</div>
-            ))}
+        let randIndex = Math.floor(Math.random() * quotes.length);
+        let randColorIndex = Math.floor(Math.random() * colors.length);
+        setColor(colors[randColorIndex])
+        setRandomQuote(quotes[randIndex])
+    }
+
+    return (
+        <div style={{backgroundColor: color, minHeight: "100vh"}}>
+
+        <div className="container pt-5">
+            <div className="jumbotron">
+                <div className="card">
+                    <div className="card-header">Inspirational Quotes</div>
+                    <div className="card-body">
+                        {randomQuote ? (
+                            <>
+                            <h5 className="card-title">- {randomQuote.author || "No author"}</h5>
+                            <p className="card-text">&quot;{randomQuote.text}&quot;</p>
+                            </>
+                        ) : (
+                            <h2>Lpading</h2>
+                        )}
+
+                        <div className="row">
+                            <button onClick={getNewQuote} className="btn btn-primary ml-3">New Quote</button>
+                            <a href={"https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + 
+                                encodeURIComponent (
+                                    '"' + randomQuote.text + '" ' + randomQuote.author
+                                )}
+                            target="_blank" className="btn btn-warning ml-3">
+                                <i className="fa fa-twitter"></i>
+                            </a>
+                            <a href="" className="btn btn-danger ml-3">
+                                <i className="fa fa-tumblr"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+
         </div>
     );
 }
